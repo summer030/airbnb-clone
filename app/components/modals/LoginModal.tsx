efda61c0280c86ebc587ef06b253b42937707ffa
summer030 <125527872+summer030@users.noreply.github.com>
2023-06-23 21:58:30 +0800

    feat: login modal

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -11,7 +11,7 @@ import useRegisterModal from "@/app/hooks/useRegisterModal";
 import useLoginModal from "@/app/hooks/useLoginModal";
 import Modal from "./Modal";
 import Heading from "../Heading";
-import Input from "@/app/input/Input";
+import Input from "@/app/inputs/Input";
 import { toast } from "react-hot-toast";
 import Button from "../Button";
 import { useRouter } from "next/navigation";
